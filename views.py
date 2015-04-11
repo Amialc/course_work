fd6dfd3c880a491fce05a91f607fec833b824cbd
VladM <pleo12@gmail.com>
2015-04-11 19:08:27 +0300

    Initial commit

---
+++ views.py
@@ -0,0 +1,11 @@
+from course import app
+from flask import render_template
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/test', methods=['GET','POST'])
+@app.route('/test/', methods=['GET','POST'])
+def test():
+    return render_template('test.html')
