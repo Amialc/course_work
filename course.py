8abf75c8d5b9a0e66630e70f62dd7936a919e95a
VladM <pleo12@gmail.com>
2015-04-11 20:35:33 +0300

    models

--- course.py
+++ course.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from datetime import timedelta
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config["REMEMBER_COOKIE_DURATION"] = timedelta(days=14)
+
+login_serializer = Serializer(app.config['SECRET_KEY'], expires_in=60)
 
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
 
 from views import *
 
--- models.py
+++ models.py
@@ -1,18 +1,67 @@
-from course import db
+from course import db, login_serializer
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    password = db.Column(db.String(120))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, email=None, password=None, j=None):
+        if j is not None:
+            self.__dict__ = j
+        else:
+            self.email = email
+            self.password = password
+
+    def get_auth_token(self):
+        """
+        Encode a secure token for cookie
+        """
+        data = [str(self.id), self.password]
+        return login_serializer.dumps(data)
+
+    def generate_auth_token(self):
+        return login_serializer.dumps({'id': self.id})
+
+    def verify_password(self, password_for_check):
+        if self.password == password_for_check:
+            return True
+        return False
+
+    @staticmethod
+    def verify_auth_token(token):
+        try:
+            data = login_serializer.loads(token)
+        except:
+            return None  # valid token, but expired
+        user = User.query.get(data['id'])
+        return user
+
+    @staticmethod
+    def get(user_id):
+        """
+        Static method to search the database and see if userid exists.  If it
+        does exist then return a User Object.  If not then return None as
+        required by Flask-Login.
+        """
+        for user in User.query.all():
+            if user.id == user_id:
+                return User(user.email, user.password)
+        return None
 
 class UserProfile(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     realname = db.Column(db.String(250))
 
-    def __init__(self, user_id, realname = None):
+
+    def __init__(self, user_id, realname=None):
         self.user_id = user_id
         self.realname = realname
 
 class Teacher(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('userprofile.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('userprofile.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
